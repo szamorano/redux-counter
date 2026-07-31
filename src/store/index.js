bb328f6d0cf3d9c46d26bed2521126324108aac4
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-31 11:48:26 -0400

    Added multiple slices to the store, and exposed multiple slice actions.

--- src/store/index.js
+++ src/store/index.js
@@ -39,8 +39,9 @@ const authSlice = createSlice({
 });
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
